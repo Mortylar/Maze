847888e2790fd621540b98ab02e123ae28d52b2a
mortylar <mortylar@student.21-school.ru>
2025-06-24 00:50:16 +0700

    Create Maze field and Maze parser

---
+++ src/model/field/field.h
@@ -0,0 +1,137 @@
+#ifndef SRC_MODEL_FIELD_FIELD_H_
+#define SRC_MODEL_FIELD_FIELD_H_
+
+#include <cstring>
+#include <iostream> //TODO remove
+
+class Field {
+  private:
+
+  public:
+  
+    virtual ~Field() = default;
+    virtual void print() = 0;
+};
+
+class MazeField: public Field {
+  private:
+    static const char K_RIGHT_BORDER = 1;
+    static const char K_DOWN_BORDER = 2;
+    char* field_;
+    size_t rows_;
+    size_t cols_;
+
+    char* Allocate(size_t n) {
+      if (n == 0) return nullptr;
+      return new char[n]();
+    }
+
+    void Clear() noexcept {
+      rows_ = 0;
+      cols_ = 0;
+      delete[] field_;
+      field_ = nullptr;
+    }
+
+    void Swap(MazeField&& other) noexcept {
+      std::swap(rows_, other.rows_);
+      std::swap(cols_, other.cols_);
+      std::swap(field_, other.field_);
+    }
+
+    void Copy(const MazeField& other) {
+      rows_ = other.rows_;
+      cols_ = other.cols_;
+      std::memcpy(field_, other.field_, rows_*cols_);
+    }
+
+    char& Get(size_t i, size_t j) {
+      return field_[i * rows_ + j];
+    }
+
+  public:
+
+  MazeField(): cols_(0), rows_(0), field_(nullptr) {}
+
+  MazeField(size_t rows, size_t cols): rows_(rows), cols_(cols) {
+    field_ = Allocate(rows*cols);
+  }
+
+  MazeField(const MazeField& other) {
+    if (&other != this) {
+      field_ = Allocate(other.rows_ * other.cols_);
+      Copy(other);
+    }
+  }
+
+  MazeField(MazeField&& other): MazeField() {
+    Swap(std::move(other));
+  }
+
+  MazeField& operator=(const MazeField& other) {
+    if (&other != this) {
+      this->Clear();
+      Copy(other);
+    }
+    return *this;
+  }
+
+  MazeField& operator=(MazeField&& other) {
+    if (&other != this) {
+      this->Clear();
+      Swap(std::move(other));
+    }
+    return *this;
+  }
+
+  ~MazeField() {
+    Clear();
+  }
+
+  size_t GetRows() { return rows_;}
+
+  size_t GetCols() { return cols_; }
+
+  bool SetRightBorder(size_t i, size_t j) {
+    if ((i < rows_) && (j < cols_)) {
+      Get(i,j) |= K_RIGHT_BORDER;
+      return true;
+    }
+    return false;
+  }
+
+  bool SetDownBorder(size_t i, size_t j) {
+    if ((i < rows_) && (j < cols_)) {
+      Get(i,j) |= K_DOWN_BORDER;
+      return true;
+    }
+    return false;
+  }
+
+  bool IsRightBorder(size_t i, size_t j) {
+    if ((i < rows_) && (j < cols_)) {
+      return Get(i, j) & K_RIGHT_BORDER;
+    }
+    return false;
+  }
+
+  bool IsDownBorder(size_t i, size_t j) {
+    if ((i < rows_) && (j < cols_)) {
+      return Get(i, j) & K_DOWN_BORDER;
+    }
+    return false;
+  }
+
+  void print() { //TODO to_string()
+    std::cout << rows_ << "x" << cols_ << std::endl;
+    for (int i = 0; i < rows_; ++i) {
+      for (int j = 0; j < cols_; ++j) {
+        std::cout << char(field_[i * rows_ + j]  + '0') << " ";
+      }
+      std::cout << std::endl;
+    }
+  }
+
+};
+
+#endif
---
+++ src/model/reader/reader.h
@@ -0,0 +1,89 @@
+#ifndef SRC_MODEL_READER_READER_H_
+#define SRC_MODEL_READER_READER_H_
+
+#include <fstream>
+
+#include "../field/field.h"
+
+class Reader {
+  protected:
+
+    std::ifstream ifs_;
+
+  public:
+    void open(const char* file) {
+      ifs_ = std::ifstream(file);
+    }
+
+    void close() {
+      ifs_.close();
+    }
+
+    virtual Field* read()=0;
+
+    virtual ~Reader() = default;
+};
+
+class MazeReader: public Reader {
+
+  private:
+  MazeField* ReadSize() {
+    int cols = 0;
+    int rows = 0;
+    if (ifs_ >> cols) {
+      if (ifs_ >> rows) {
+        MazeField* maze = new MazeField(cols, rows);
+        return maze;
+      }
+    }
+    throw std::invalid_argument("Can't read from file\n");
+  }
+
+  void ReadRightBorder(MazeField& maze) {
+    for (int i = 0; i < maze.GetRows(); ++i) {
+      for (int j = 0; j < maze.GetCols(); ++j) {
+        int x = 0;
+        if (ifs_ >> x) {
+          if (x > 0) maze.SetRightBorder(i,j);
+        } else {
+          throw std::invalid_argument("Can't read from file\n");
+        }
+      }
+    }
+  }
+
+  void ReadDownBorder(MazeField& maze) {
+    for (int i = 0; i < maze.GetRows(); ++i) {
+      for (int j = 0; j < maze.GetCols(); ++j) {
+        int x = 0;
+        if (ifs_ >> x) {
+          if (x > 0) maze.SetDownBorder(i,j);
+        } else {
+          throw std::invalid_argument("Can't read from file\n");
+        }
+      }
+    }
+  }
+
+  public:
+
+  MazeReader() = default;
+
+  MazeReader(const char* file_name) {
+    open(file_name);
+    if (!ifs_.is_open()) throw std::invalid_argument("Can't open file\n");
+  }
+
+  Field* read() {
+    MazeField* maze = ReadSize();
+    ReadRightBorder(*maze);
+    ReadDownBorder(*maze);
+    return maze;
+  }
+
+  ~MazeReader() {
+    close();
+  }
+};
+
+#endif
